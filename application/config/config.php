93bd816dcfa10e9e7ae728c46ef4e93c4481555c
bishyan <bishiyan2008@163.com>
2022-06-16 09:45:08 +0800

    ..

--- application/config/config.php
+++ application/config/config.php
@@ -8,7 +8,7 @@ return array(
 		'host'		=> 'localhost',
 		'port'		=> '3306',
 		'username'	=> 'root',
-		'password'	=> 'root',
+		'password'	=> '1234abcd',
 		'charset'	=> 'utf8',
 		'dbname'	=> 'xshop02',
 	),
